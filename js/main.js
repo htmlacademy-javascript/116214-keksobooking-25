b57f588f8c289cf926c4dfc86d79b7c2172dd046
Vakhtang Tivadze <vaxotivadze@gmail.com>
2022-02-19 03:03:47 +0400

    Rrealize function getRandomFloat(min, max, decimalCount)

--- js/main.js
+++ js/main.js
@@ -11,3 +11,14 @@ function getRandomInteger(min, max) {
 }
 
 getRandomInteger();
+
+function getRandomFloat(min, max, decimalCount) {
+  const decimalFactor = Math.pow(10, decimalCount);
+
+  min = Math.round(min * decimalFactor);
+  max = Math.round(max * decimalFactor);
+
+  return getRandomInteger(min, max) / decimalFactor;
+}
+
+getRandomFloat();
