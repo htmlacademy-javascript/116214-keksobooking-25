2c35d336d84289d9884697b54cf24ee669716850
Vakhtang Tivadze <vaxotivadze@gmail.com>
2022-02-26 17:17:17 +0400

    Make some other refactorings

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 const SIMILAR_ADDS_COUNT = 10;
 const userIds = Array.from(new Array(SIMILAR_ADDS_COUNT), (v, i) => `0${i + 1}`.slice(-2));
-
+const hours = ['12:00', '13:00', '14:00'];
 const realtyTypes = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
 const features = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator'];
 const photos = [
@@ -8,7 +8,6 @@ const photos = [
   'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
   'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg'
 ];
-const hours = ['12:00', '13:00', '14:00'];
 
 const getRandomPositiveInteger = (num1, num2) => {
   const min = Math.ceil(Math.min(Math.abs(num1), Math.abs(num2)));
@@ -37,36 +36,28 @@ const getArrayRandomNumberElements = (arr) => {
   return shuffledArray.slice(0, getRandomPositiveInteger(0, arr.length - 1));
 };
 
-const getAuthor = (userId) => ({avatar: `img/avatars/user${userId}.png`});
-
-const getLocation = () => ({
-  lat: getRandomPositiveFloat(35.65, 35.7, 5),
-  lng: getRandomPositiveFloat(139.7, 139.8, 5)
-});
-
-const getOffer = (userId) => ({
-  title: `title${userId}`,
-  address: '',
-  price: getRandomPositiveInteger(100, 1000),
-  type: getArrayRandomElement(realtyTypes),
-  rooms: getRandomPositiveInteger(1, 10),
-  guests: getRandomPositiveInteger(1, 20),
-  checkin: getArrayRandomElement(hours),
-  checkout: getArrayRandomElement(hours),
-  features: getArrayRandomNumberElements(features),
-  description: `description${userId}`,
-  photos: getArrayRandomNumberElements(photos)
-});
-
 const generateMockData = () => {
   const userId = getArrayRandomUniqueElement(userIds);
-  const add = {
-    author: getAuthor(userId),
-    offer: getOffer(userId),
-    location: getLocation()
+  const lat = getRandomPositiveFloat(35.65, 35.7, 5);
+  const lng = getRandomPositiveFloat(139.7, 139.8, 5);
+
+  return {
+    author: {avatar: `img/avatars/user${userId}.png`},
+    offer: {
+      title: `title${userId}`,
+      address: `${lat}, ${lng}`,
+      price: getRandomPositiveInteger(100, 1000),
+      type: getArrayRandomElement(realtyTypes),
+      rooms: getRandomPositiveInteger(1, 10),
+      guests: getRandomPositiveInteger(1, 20),
+      checkin: getArrayRandomElement(hours),
+      checkout: getArrayRandomElement(hours),
+      features: getArrayRandomNumberElements(features),
+      description: `description${userId}`,
+      photos: getArrayRandomNumberElements(photos)
+    },
+    location: {lat, lng}
   };
-  add.offer.address = `${add.location.lat}, ${add.location.lng}`;
-  return add;
 };
 
 const similarAdds = Array.from({length: SIMILAR_ADDS_COUNT}, generateMockData);
