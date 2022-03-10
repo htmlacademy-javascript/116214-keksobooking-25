228641ff72626471738ffe98917db6aa38c07748
Vakhtang Tivadze <vaxotivadze@gmail.com>
2022-03-10 17:57:22 +0400

    Make some additional refactoring of page and form disabling functions

--- js/main.js
+++ js/main.js
@@ -1,8 +1,11 @@
 import {cards} from './generateCards.js';
 import { togglePageActivity } from './form.js';
 
-setTimeout(() => togglePageActivity(false), 2000);
-setTimeout(() => togglePageActivity(true), 5000);
+const DISABLE = false;
+const ENABLE = true;
+
+setTimeout(() => togglePageActivity(DISABLE), 2000);
+setTimeout(() => togglePageActivity(ENABLE), 5000);
 
 const mapCanvas = document.querySelector('#map-canvas');
 mapCanvas.appendChild(cards[0]);
--- js/util.js
+++ js/util.js
@@ -80,10 +80,10 @@ const disableForm = (className, status) => {
   const form = document.querySelector(`.${className}`);
 
   Array.from(form.elements).forEach((item) => {
-    item.disabled = status;
+    item.disabled = !status;
   });
   Array.from(form.querySelector('fieldset')).forEach((item) => {
-    item.disabled = status;
+    item.disabled = !status;
   });
 
   form.classList[status ? 'remove' : 'add'](`${className}--disabled`);
