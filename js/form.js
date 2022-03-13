33087d56ab5e99e55c4a83bcc1fe926debc2d112
Vakhtang Tivadze <vaxotivadze@gmail.com>
2022-03-14 00:42:47 +0400

    Add event listeners for type, timein and timeout elements of form 'ad-form'

--- js/form.js
+++ js/form.js
@@ -1,5 +1,19 @@
 const formClassNames = ['ad-form', 'map__filters'];
 
+const minPricesPerNightByType = {
+  bungalow: 0,
+  flat: 1000,
+  hotel: 3000,
+  house: 5000,
+  palace: 10000
+};
+
+const adForm = document.querySelector('.ad-form');
+const type = adForm.querySelector('#type');
+const price = adForm.querySelector('#price');
+const timein = adForm.querySelector('#timein');
+const timeout = adForm.querySelector('#timeout');
+
 const disableForm = (className, status) => {
   const form = document.querySelector(`.${className}`);
 
@@ -15,5 +29,17 @@ const disableForm = (className, status) => {
 
 const activateForm = (className) => disableForm(className, true);
 const deactivateForm = (className) => disableForm(className, false);
+
+type.addEventListener('change', () => {
+  price.setAttribute('min', minPricesPerNightByType[type.value]);
+});
+
+timein.addEventListener('change', () => {
+  timeout.value = timein.value;
+});
+
+timeout.addEventListener('change', () => {
+  timein.value = timeout.value;
+});
 
 export {formClassNames, activateForm, deactivateForm};
