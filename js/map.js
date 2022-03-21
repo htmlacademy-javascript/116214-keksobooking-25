7dfa82fe89caec10548a9d2419bd04e2028feb7c
Vakhtang Tivadze <vaxotivadze@gmail.com>
2022-03-21 09:57:29 +0400

    Realize displaying of map

--- js/map.js
+++ js/map.js
@@ -1,5 +1,12 @@
-L.map('map-canvas')
+const map = L.map('map-canvas')
   .setView({
     lat: 35.693,
     lng: 139.746,
   }, 10);
+
+L.tileLayer(
+  'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+  {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  },
+).addTo(map);
