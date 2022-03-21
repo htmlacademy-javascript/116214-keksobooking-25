997041b424aa96f6c304ddca1559fa51aac0789d
Vakhtang Tivadze <vaxotivadze@gmail.com>
2022-03-21 11:03:44 +0400

    Change main marker icon design

--- js/map.js
+++ js/map.js
@@ -1,8 +1,10 @@
+const mapCenter = {
+  lat: 35.693,
+  lng: 139.746
+};
+
 const map = L.map('map-canvas')
-  .setView({
-    lat: 35.693,
-    lng: 139.746,
-  }, 10);
+  .setView(mapCenter, 10);
 
 L.tileLayer(
   'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
@@ -10,5 +12,20 @@ L.tileLayer(
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
   },
 ).addTo(map);
+
+const mainPinIcon = L.icon({
+  iconUrl: './img/main-pin.svg',
+  iconSize: [52, 52],
+  inconAnchor: [26, 52]
+});
+
+const marker = L.marker(
+  mapCenter,
+  {
+    icon: mainPinIcon
+  }
+);
+
+marker.addTo(map);
 
 export {map};
