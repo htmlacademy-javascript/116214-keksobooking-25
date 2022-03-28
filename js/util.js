22664170479b30e82f0c4cb344e14e26f586a396
Vakhtang Tivadze <vaxotivadze@gmail.com>
2022-03-28 08:20:53 +0400

    Remove unnecessary functions from 'utils.js'

--- js/util.js
+++ js/util.js
@@ -1,35 +1,3 @@
-const getRandomPositiveInteger = (num1, num2) => {
-  const min = Math.ceil(Math.min(Math.abs(num1), Math.abs(num2)));
-  const max = Math.floor(Math.max(Math.abs(num1), Math.abs(num2)));
-  const result = Math.random() * (max - min + 1) + min;
-  return  Math.floor(result);
-};
-
-const getRandomPositiveFloat = (num1, num2, digits = 1) => {
-  const min = Math.min(Math.abs(num1), Math.abs(num2));
-  const max = Math.max(Math.abs(num1), Math.abs(num2));
-  const result = Math.random() * (max - min) + min;
-  return +result.toFixed(digits);
-};
-
-const getArrayRandomElement = (arr) => arr[getRandomPositiveInteger(0, arr.length - 1)];
-
-const getArrayRandomNumberElements = (arr) => {
-  const shuffledArray = arr.sort(() => 0.5 - Math.random());
-  return shuffledArray.slice(0, getRandomPositiveInteger(0, arr.length - 1));
-};
-
-const generateId = (max) => {
-  let current = 0;
-  return () => {
-    current = current + 1;
-    if (current <= max) {
-      return `0${current}`.slice(-2);
-    }
-    return NaN;
-  };
-};
-
 const addContent = {
   text(element, content) {
     element.textContent = content;
@@ -79,11 +47,6 @@ const formatCapacity = (rooms, guests) => rooms && guests ? `${rooms} комна
 const formatTime = (checkin, checkout) => checkin && checkout ? `Заезд после ${checkin}, выезд до ${checkout}` : null;
 
 export {
-  getRandomPositiveInteger,
-  getRandomPositiveFloat,
-  getArrayRandomElement,
-  getArrayRandomNumberElements,
-  generateId,
   populateElement,
   formatPrice,
   formatCapacity,
