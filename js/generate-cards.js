ec27b1bc2722ca946885f1ae68baa98c36697553
Vakhtang Tivadze <vaxotivadze@gmail.com>
2022-04-18 06:33:22 +0400

    Delete unnecessary type casting

--- js/generate-cards.js
+++ js/generate-cards.js
@@ -22,22 +22,22 @@ const generateCard = (author, offer) => {
   const card = cardTemplate.cloneNode(true);
 
   const titleItem = card.querySelector('.popup__title');
-  populateElement(titleItem, String(title), 'insertAsText');
+  populateElement(titleItem, title, 'insertAsText');
 
   const addressItem = card.querySelector('.popup__text--address');
-  populateElement(addressItem, String(address), 'insertAsText');
+  populateElement(addressItem, address, 'insertAsText');
 
   const priceItem = card.querySelector('.popup__text--price');
   populateElement(priceItem, formatPrice(parseInt(price, 10)), 'insertAsHtml');
 
   const realtyTypeItem = card.querySelector('.popup__type');
-  populateElement(realtyTypeItem, REALTY_TYPES[String(type)], 'insertAsText');
+  populateElement(realtyTypeItem, REALTY_TYPES[type], 'insertAsText');
 
   const capacityItem = card.querySelector('.popup__text--capacity');
   populateElement(capacityItem, formatCapacity(parseInt(rooms, 10), parseInt(guests, 10)), 'insertAsText');
 
   const timeItem = card.querySelector('.popup__text--time');
-  populateElement(timeItem, formatTime(String(checkin), String(checkout)), 'insertAsText');
+  populateElement(timeItem, formatTime(checkin, checkout), 'insertAsText');
 
   const featureContainer = card.querySelector('.popup__features');
   const featureList = featureContainer.querySelectorAll('.popup__feature');
@@ -48,7 +48,7 @@ const generateCard = (author, offer) => {
   }
 
   const descriptionItem = card.querySelector('.popup__description');
-  populateElement(descriptionItem, String(description), 'insertAsText');
+  populateElement(descriptionItem, description, 'insertAsText');
 
   const photoItemsContainer = card.querySelector('.popup__photos');
   const photoTemplate = photoItemsContainer.querySelector('.popup__photo');
@@ -57,7 +57,7 @@ const generateCard = (author, offer) => {
 
   const avatarItem = card.querySelector('.popup__avatar');
   if (author.avatar) {
-    avatarItem.src = String(author.avatar);
+    avatarItem.src = author.avatar;
   } else {
     avatarItem.classList.add('hidden');
   }
